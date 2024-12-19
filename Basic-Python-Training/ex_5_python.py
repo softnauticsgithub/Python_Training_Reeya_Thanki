44851eea6a77c4637eeeb95ddded2551f079f683
reeya thanki <reeya.thanki@moschip.com>
2024-12-19 16:14:18 +0530

    ex_5_python_to_ex_9

---
+++ Basic-Python-Training/ex_5_python.py
@@ -0,0 +1,25 @@
+#Topic : BASIC PYTHON
+#Exercise Number : 2
+##PROBLEM STATEMENT:
+#   Read a given string, change the character at a given index and then print the modified string.
+# Python Version : 3.7
+"""
+    - Input Format
+        The first line contains a string, .
+        The next line contains an integer , denoting the index location
+         and a character separated by a space.
+    - Output Format
+        Using any of the methods explained above, replace the character at index with character .
+    - Sample Input
+        abracadabra
+        5 k
+    - Sample Output
+        abrackdabra
+"""
+name=input("Enter one String:")
+index,ch=input().split(" ")
+index=int(index)
+x=list(name)
+x[index]=ch
+RES="".join(x)
+print(RES)
